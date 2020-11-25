c004e722a632eb62bd078b97a756f577eeb414b2
Tim Schumacher <timschumi@gmx.de>
2020-11-25 16:46:14 +0100

    android: Fetch configs from last supported commit

--- android/configs/android-fetch-configs.sh
+++ android/configs/android-fetch-configs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl https://android.googlesource.com/kernel/configs/+archive/master/android-3.18.tar.gz | tar xzv
+curl https://android.googlesource.com/kernel/configs/+archive/e97c5487372cb8e73e77c4f63d3011c90580dbdd/android-3.18.tar.gz | tar xzv
 
